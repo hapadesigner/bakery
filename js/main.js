b84daf926cff64d270ded8030a847c225d52790e
Paul Gay <paulrgay1@gmail.com>
2017-11-30 16:06:35 -0500

    fixed headers

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 document.onscroll = function () {
-  const px = document.scrollTop
+  const px = document.documentElement.scrollTop
 
 if (px > 20) {
   document.querySelector('.navigation').style.display = 'none';
   document.querySelector('.navigation-sm').style.display = 'block';
- 
+
 
 } else {
   document.querySelector('.navigation-sm').style.display = 'none';
